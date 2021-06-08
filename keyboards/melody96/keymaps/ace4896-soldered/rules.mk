c3aa51f1bf821aac1b94b3e5cdae74bf35925d49
Jon Pacheco <jon.pacheco@tutanota.com>
2021-06-08 20:42:54 +0100

    feat: use symmetric, deferred, per-key debounce

--- keyboards/melody96/keymaps/ace4896-soldered/rules.mk
+++ keyboards/melody96/keymaps/ace4896-soldered/rules.mk
@@ -1,5 +1,8 @@
-# NOTE: VIA inteferes with bootmagic, disabled for now
-# VIA_ENABLE = yes
+# Use symmetric, deferred, per-key debounce
+DEBOUNCE_TYPE = sym_defer_pk
 
 # Reduces firmware size
 LTO_ENABLE = yes
+
+# NOTE: VIA interferes with bootmagic + NKRO, disabled for now
+# VIA_ENABLE = yes
